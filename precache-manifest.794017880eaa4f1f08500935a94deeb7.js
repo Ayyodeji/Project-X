71424eb57a6aeb4055502c1408ed1de0e675c035
Ayodeji Ajayi <ajayi.ayyodeji@gmail.com>
2023-08-21 00:50:17 +0100

    Updates

---
+++ precache-manifest.794017880eaa4f1f08500935a94deeb7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2ffed1c7852af1fde400d03c7f5faa",
+    "url": "/Project-X/index.html"
+  },
+  {
+    "revision": "1e4603a295548b35e88c",
+    "url": "/Project-X/static/css/main.6318fca5.chunk.css"
+  },
+  {
+    "revision": "905aa05b977935be4117",
+    "url": "/Project-X/static/js/2.16adf028.chunk.js"
+  },
+  {
+    "revision": "21230c2e18e97caedb8c7bf15252cd93",
+    "url": "/Project-X/static/js/2.16adf028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71b0cb446ecc54f1acdd",
+    "url": "/Project-X/static/js/3.8a5307b3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Project-X/static/js/3.8a5307b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e4603a295548b35e88c",
+    "url": "/Project-X/static/js/main.4b13c8a7.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Project-X/static/js/main.4b13c8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "540d3e3a6bc257bdd5c6",
+    "url": "/Project-X/static/js/runtime-main.35664797.js"
+  }
+]);
